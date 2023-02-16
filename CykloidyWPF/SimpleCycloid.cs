aad02b7f80b969777ce4e89d80aedb1a1770656d
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2023-02-16 22:36:12 +0100

    Save my soul from despair

--- CykloidyWPF/SimpleCycloid.cs
+++ CykloidyWPF/SimpleCycloid.cs
@@ -1,24 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 
 namespace CykloidyWPF
 {
     class SimpleCycloid
     {
         double r = 1;      // radius of rolling circle
-        double t;          // time parameter
         public double x, y;       // x and y coordinates of point on cycloid
 
         public SimpleCycloid(double radius, double x, double y)
         {
             this.r = radius;
+            this.x = x;
+            this.y = y;
         }
 
-        public void Calculate()
+        private List<PointD> GetCycloidPoints()
         {
-            x = r * (t / 100 - Math.Sin(t) + 30);
-            y = r * (1 - Math.Cos(t)) + 50;
+            List<PointD> points = new List<PointD>();
+
+            double step = 360D;
+            double stepsTaken = 0D;
+
+            double a = r;
+            double x, y;
+
+            while (stepsTaken < 360)
+            {
+                x = step * a * ((stepsTaken / 100) - Math.Sin(stepsTaken * (Math.PI / 180D)));
+                y = step * a * (1 - Math.Cos(stepsTaken * (Math.PI / 180D)));
+                points.Add(new PointD(x, y));
+                stepsTaken += 1;
+            }
+
+            return points;
         }
     }
 }
